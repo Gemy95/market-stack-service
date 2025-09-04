3648f5821d211f1f6e0e108ab62dd03c739aefb6
ali.gamal <ali.gamal95880@gmail.com>
2025-09-05 01:09:00 +0300

    feat/add-cache-manager-using-redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { configuration } from '@App/config/configuration';
 import { validate } from '@App/config/env.validation';
 import { AppController } from '@App/app.controller';
 import { AppService } from '@App/app.service';
 import { MarketStackModule } from '@App/modules/market-stack/market-stack.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { createKeyv } from '@keyv/redis';
+import { Keyv } from 'keyv';
+import { CacheableMemory } from 'cacheable';
 
 @Module({
   imports: [
@@ -12,6 +16,21 @@ import { MarketStackModule } from '@App/modules/market-stack/market-stack.module
       isGlobal: true,
       load: [configuration],
       validate,
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        stores: [
+          new Keyv({
+            store: new CacheableMemory(),
+          }),
+          createKeyv(
+            `redis://${configService.get<string>('REDIS_USERNAME')}:${configService.get<string>('REDIS_PASSWORD')}@${configService.get<string>('REDIS_HOST')}:${configService.get<number>('REDIS_PORT')}`,
+          ),
+        ],
+      }),
     }),
     MarketStackModule,
   ],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,6 +3,7 @@ const DEFAULT_SERVER_PORT = 5000;
 export interface Configuration {
   app: AppSetting;
   services: Services;
+  redis: Redis;
 }
 
 export interface AppSetting {
@@ -19,6 +20,13 @@ export interface MarketStack {
   apiKey: string;
 }
 
+export interface Redis {
+  host: string;
+  port: number;
+  username?: string;
+  password?: string;
+}
+
 export const configuration = (): Configuration => {
   const defaultConfiguration: Configuration = {
     app: {
@@ -30,6 +38,12 @@ export const configuration = (): Configuration => {
         url: process.env.MARKET_STACK_BASE_URL,
         apiKey: process.env.MARKET_STACK_API_KEY,
       },
+    },
+    redis: {
+      host: process.env.REDIS_HOST,
+      port: +process.env.REDIS_PORT,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
     },
   };
   return defaultConfiguration;
